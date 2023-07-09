71858aaca05152ebea1b2c9f931b1fa3d9278e1c
RodrigoIniesta <iniesta@ciencias.unam.mx>
2023-07-09 10:57:07 -0500

    Fin de la cuarta clase

--- 04_Analisis_Market_Data.py
+++ 04_Analisis_Market_Data.py
@@ -13,44 +13,46 @@ import scipy
 import importlib
 from scipy.stats import skew,kurtosis,chi2
 
-#print("First code")
 
-#simulating random variables
-nb_sims=10**6
-df=5
-lam=2
-dist_name= 'normal'
-dist_type = 'sim RV'
 
-if dist_name == 'normal':
-    x=np.random.standard_normal(nb_sims)
-    x_desc= dist_type + ' ' + dist_name
-elif dist_name == 'exponential':
-    x=np.random.exponential(lam,nb_sims)
-    x_desc= dist_type + ' ' + dist_name
-elif dist_name == 'uniform':
-    x=np.random.uniform(0,2,nb_sims)
-    x_desc= dist_type + ' ' + dist_name
-elif dist_name == 'student':
-    x=np.random.standard_t(df,nb_sims)
-    x_desc= dist_type + ' ' + dist_name+' | df='+str(df) 
-elif dist_name == 'chi-squared':
-    x=np.random.standard_t(df,nb_sims)
-    x_desc= dist_type + ' ' + dist_name+' | df='+str(df)
+#### get market data ####
+file='BBVA.MC'
+#file='^VIX'
+#file='MXN=X'
+path = 'C:\\Users\\rodrigo\\Documents\\Finanzas Cuantitativas con Python\\4_Analisis_Market_Data\\'+file+'.csv'
+raw_data = pd.read_csv(path)
+#x = raw_data['Close'] #Formato Series (indice mas valores)
+x = raw_data['Close'].values #Formato arreglo de valores (solo valores)
+#data_lenght=len(x) #obtener la longitud del vectos = cantidad de datos
+data_lenght=raw_data.shape [0] #longitud desde las caracteristicas del arreglo
 
-'''
-Todays goal - Create Jarque - Bera normality test
-'''
-x_variance=np.var(x)
+###Create returns of the series in another file###
+t = pd.DataFrame()
+#t['date']=raw_data['Date']
+#type(t['date'][0]) #checking the data type
+t['date'] = pd.to_datetime(raw_data['Date'],dayfirst=True) #convert the data type to date instead of string
+t['close'] = raw_data['Close']
+t.sort_values(by='date',ascending = True)
+for i in range(data_lenght-1):
+    if pd.isna(t['close'][i]): #Check if the data is nan y fill it withe last one
+        t['close'][i]=t['close'][i-1]
+t['close_shift_1'] = t['close'].shift(1) #create a column with the shifted data by row
+t['return']=t['close']/t['close_shift_1']-1 
+t=t.dropna() #erase row that have nan
+t=t.reset_index(drop=True) #to reste the index of the table as we erase some lines
+x=t['return'].values
+
+##JarqueVera###
+x_variance=np.var(x) 
 x_skew = skew(x)
 x_kurtosis = kurtosis(x) #Esta Kurtosis ya esta en exceso (K-3)
-x_jarque_bera = nb_sims/6*(x_skew**2+1/4*x_kurtosis**2)
+x_jarque_bera = data_lenght/6*(x_skew**2+1/4*x_kurtosis**2)
 p_value = 1 - chi2.cdf(x_jarque_bera,df=2)
 x_is_normal = (p_value>0.05)  #para revisar si el p_value es mayor 1-.95
 
 #jb_list = [] #create a list
 #jb_list.append(x_jarque_bera) #Para ir apilando los resultados correr varias veces
-
+x_desc = 'market data ' +  file
 print('kurtosis is ' + str(x_kurtosis)) 
 print('skew is ' + str(x_skew))
 print('Jarque-Bera statistic is ' + str(x_jarque_bera))
